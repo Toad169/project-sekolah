ff5f951906da4bef707b6040467b9f77c95c252a
Toad169 <mamangpunten893@gmail.com>
2026-02-27 09:58:48 +0700

    some tweaks

--- index.php
+++ index.php
@@ -204,7 +204,7 @@ for ($i = 6; $i >= 0; $i--) {
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Karyawan</div>
+                      <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Siswa</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?=$karyawan?></div>
                     </div>
                     <div class="col-auto">
